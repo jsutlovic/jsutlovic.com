831fb35f245dd06671c9aae1f6f65edcd77aad8b
Jero Sutlovic <jsutlovic@gmail.com>
2012-05-22 10:59:08 -0400

    Add links to all rendered contexts with rr, create page and contact views

--- main/views.py
+++ main/views.py
@@ -4,14 +4,46 @@ from django.shortcuts             import render_to_response, get_object_or_404, 
 from django.template              import Context, loader, RequestContext
 from django.http                  import HttpResponse, Http404
 
-from ocds.ocds_main.models import *
+from main.models import *
 
 
 def rr(template, context, request):
+    links = {"primary": SiteLink.objects.filter(primaryLinks=True, disabled=False).order_by('-weight'),
+             "secondary": SiteLink.objects.filter(secondaryLinks=True, disabled=False).order_by('-weight'),
+            }
+    
+    if "page" in context and isinstance(context["page"], Page):
+        links["page"] = context["page"].links.filter(disabled=False).order_by('-weight')
+    
+    context["links"] = links
+    
     return render_to_response(template, context, context_instance=RequestContext(request))
 
 def about(request):
-    page = "about"
+    page = get_object_or_404( Page, name="about" )
+    
+    return rr("page.djhtml", {"page": page}, request)
 
-    r = get_object_or_404( Page, name=page )
-    return rr("page.html", {"page": r}, request)
+
+def contact(request):
+    page = get_object_or_404( Page, name="contact" )
+    
+    details = ContactDetail.objects.all().order_by('-weight')
+    
+    return rr("contact.djhtml", {"page": page, "details": details}, request)
+
+
+def page(request, name=""):
+    if not name:
+        return about(request)
+    
+    #Switch some names:
+    if name == "about":
+        return redirect("jsutlovic-index")
+    elif name == "contact":
+        return contact(request)
+    
+    rpage = get_object_or_404( Page, name=name )
+    
+    return rr("page.djhtml", {"page": rpage}, request)
+
