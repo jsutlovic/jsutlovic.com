6bb353f2701b636b61464063d2f0ae9c5ff5aac6
Jero Sutlovic <jsutlovic@gmail.com>
2012-05-22 10:52:54 -0400

    Add admin and static urls, and about/contact page urls

--- JSutlovic/urls.py
+++ JSutlovic/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -9,12 +10,18 @@ urlpatterns = patterns('',
     # url(r'^$', 'JSutlovic.views.home', name='home'),
     # url(r'^JSutlovic/', include('JSutlovic.foo.urls')),
     
-    url(r'^$', 'main.views.about', name='jsutlovic-about'),
+    url(r'^$', 'main.views.about', name='jsutlovic-index'),
     url(r'^about$', 'main.views.about', name='jsutlovic-about'),
-
+    url(r'^contact^', 'main.views.contact', name='jsutlovic-contact'),
+    
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+
+    #Page catch-all
+    url(r'^(?P<name>\S+)$', 'main.views.page', name='jsutlovic-page'),
 )
+
+urlpatterns += staticfiles_urlpatterns()
