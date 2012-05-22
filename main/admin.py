b6ece9cf0c060e0a9ad919fe808fd35e37bc3e0a
Jero Sutlovic <jsutlovic@gmail.com>
2012-05-22 10:56:07 -0400

    Register SiteLink and ContactDetail with Django admin so we can edit it through the admin interface

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,7 @@
-from main.models import Page, PageLink
+from main.models import Page, PageLink, SiteLink, ContactDetail
 from django.contrib import admin
 
 admin.site.register(Page)
 admin.site.register(PageLink)
-
+admin.site.register(SiteLink)
+admin.site.register(ContactDetail)
