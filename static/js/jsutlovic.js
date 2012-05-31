987a8a605aceefc9056653f4559b8954268f5161
Jero Sutlovic <jsutlovic@gmail.com>
2012-05-31 10:47:44 -0400

    Move scroller init into a new file, with fancybox init

---
+++ static/js/jsutlovic.js
@@ -0,0 +1,62 @@
+/** Some basic JS for all pages on jsutlovic.com */
+
+$(function(){
+  /* 
+   * Use jQuery jScrollPane script for scrolling "scrollable" content:
+   * Shift the vertical gutter (where the scroller appears) 8 pixels left,
+   * and refresh when the browser resizes
+   */
+  $('div.main.container.scroll section article.scrollable').each(
+  function() {
+    $(this).jScrollPane(
+      {
+        verticalGutter: -8
+      }
+    );
+    var api = $(this).data('jsp');
+    var throttleTimeout;
+    $(window).bind(
+      'resize',
+      function()
+      {
+        if ($.browser.msie) {
+          // IE fires multiple resize events while you are dragging the browser window which
+          // causes it to crash if you try to update the scrollpane on every one. So we need
+          // to throttle it to fire a maximum of once every 50 milliseconds...
+          if (!throttleTimeout) {
+            throttleTimeout = setTimeout(
+              function()
+              {
+                api.reinitialise();
+                throttleTimeout = null;
+              },
+              50
+            );
+          }
+        } else {
+          api.reinitialise();
+        }
+      }
+    );
+  });
+
+
+  /*
+   * Fancybox init:
+   * Use buttons, at the bottom with the title outside the image
+   * modal box, only use previous, next, scale and close buttons
+   * Don't allow mouseWheel (gets annoying trying to scroll images)
+   */
+  $(".fancybox").fancybox({
+    prevEffect    : 'none',
+    nextEffect    : 'none',
+    closeBtn    : false,
+    mouseWheel  : false,
+    helpers   : {
+      title : { type : 'outside' },
+      buttons : {
+        position: 'bottom',
+        tpl: '<div id="fancybox-buttons"><ul><li><a class="btnPrev" title="Previous" href="javascript:;"></a></li><li><a class="btnNext" title="Next" href="javascript:;"></a></li><li><a class="btnToggle" title="Toggle size" href="javascript:;"></a></li><li><a class="btnClose" title="Close" href="javascript:jQuery.fancybox.close();"></a></li></ul></div>'},
+    }
+  });
+});
