1a6886accb5890b96e2bf9a284c385c42d4b9905
Jero Sutlovic <jsutlovic@gmail.com>
2013-01-13 02:43:30 -0500

    PEP8

--- main/models.py
+++ main/models.py
@@ -1,21 +1,28 @@
 from django.db import models
-from django.db.models import *
+from django.db.models import Model, SlugField, CharField, TextField, \
+    BooleanField, IntegerField, ForeignKey, OneToOneField, FileField, \
+    ManyToManyField, DateField, URLField, ImageField
 
 #Options for date formatting
 DATE_CHOICES = (('M Y', "Month and Year"), ('Y', "Year only"))
 nums = tuple(range(100, -101, -1))
 NUM_CHOICES = tuple(zip(nums, [str(n) for n in nums]))
 
+
 #Find image path utility
 def get_image_path(instance, filename):
     if isinstance(instance, ProjectImage):
-        return "images/%(project)s/%(filename)s" % {'project': instance.project.name, 'filename': filename}
+        return "images/%(project)s/%(filename)s" % \
+            {'project': instance.project.name, 'filename': filename}
     elif isinstance(instance, ProjectIcon):
-        return "images/%(project)s/icon.png" % {'project': instance.project.name}
+        return "images/%(project)s/icon.png" % \
+            {'project': instance.project.name}
+
 
 def get_pdf_path(instance, filename):
     if isinstance(instance, Resume):
         return "files/resume/%(filename)s.pdf" % {'filename': instance.name}
+
 
 class Page(Model):
     #The page name/slug
@@ -23,53 +30,61 @@ class Page(Model):
     #The page title
     title = CharField(max_length=256, help_text="The page title")
     #A description of the page
-    description = CharField(max_length=256, help_text="A description of the page (used in meta fields)")
-    
+    description = CharField(
+        max_length=256,
+        help_text="A description of the page (used in meta fields)")
+
     #Custom header content
     header_content = TextField(blank=True, help_text="Custom header content")
-    
+
     #The actual content
     content = TextField(help_text="The page content")
-    
-    #Whether the page will be full-height with content scrolling or expand the page
-    scrollable = BooleanField(default=False, help_text="full height with scroll or expand to fit")
-    
+
+    #Whether the page will be full-height with content scrolling
+    #or whether the page will expand with the content
+    scrollable = BooleanField(
+        default=False,
+        help_text="full height with scroll or expand to fit")
+
     #display this in the admin interface
     def __unicode__(self):
         return self.name
-    
+
     @models.permalink
     def get_absolute_url(self):
         return ('jsutlovic-page', [str(self.name)])
-        
+
 
 class Link(Model):
     """Common link fields"""
-    
+
     #The link text
     name = CharField(max_length=128, help_text="Link text")
-    
+
     #The words displayed
     title = CharField(max_length=128, help_text="Link title (hover text)")
-    
+
     #The URL the site link refers to
     url = CharField(max_length=512, help_text="URL the link refers to")
-    
+
     #Weight of the link (left to right order)
-    weight = IntegerField(default=0, choices=NUM_CHOICES, help_text="Weight (display order)")
+    weight = IntegerField(
+        default=0,
+        choices=NUM_CHOICES,
+        help_text="Weight (display order)")
 
     #Disable the link?
     disabled = BooleanField(default=False, help_text="Disable link")
-    
+
     class Meta:
-        abstract=True
-        ordering=['-weight', 'name']
-    
+        abstract = True
+        ordering = ['-weight', 'name']
+
 
 class PageLink(Link):
     #The page that the link is related to
     page = ForeignKey('Page', related_name="links", help_text="Related page")
-    
+
     #Display this in the admin interface
     def __unicode__(self):
         return ' - '.join([self.page.title, self.name])
@@ -77,14 +92,22 @@ class PageLink(Link):
 
 class SiteLink(Link):
     #Is it a Primary link? (top of page)
-    primaryLinks = BooleanField(default=False, help_text="Primary link (top of page)")
-    
+    primaryLinks = BooleanField(
+        default=False,
+        help_text="Primary link (top of page)")
+
     #Secondary link? (bottom of page)
-    secondaryLinks = BooleanField(default=False, help_text="Secondary link (bottom of page)")
-    
+    secondaryLinks = BooleanField(
+        default=False,
+        help_text="Secondary link (bottom of page)")
+
     #Related pages
-    page = OneToOneField('Page', null=True, blank=True, help_text="Related page")
-    
+    page = OneToOneField(
+        'Page',
+        null=True,
+        blank=True,
+        help_text="Related page")
+
     def __unicode__(self):
         return self.name
 
@@ -92,128 +115,173 @@ class SiteLink(Link):
 class ContactDetail(Model):
     #The type of the contact detail (website, email, etc)
     type = CharField(max_length=64, help_text="Type, e.g. website, email, url")
-    
+
     #The value (text displayed)
     value = CharField(max_length=256, help_text="Text displayed")
-    
+
     #If it's a link, the URL to go to
     url = CharField(max_length=512, blank=True, help_text="Associated URL")
-    
+
     #The weight (vertical position)
-    weight = IntegerField(default=0, choices=NUM_CHOICES, help_text="Weight (order displayed)")
-    
+    weight = IntegerField(
+        default=0,
+        choices=NUM_CHOICES,
+        help_text="Weight (order displayed)")
+
     #For schema.org microdata markup
-    itemprop = CharField(max_length=32, blank=True, help_text="For schema.org microdata")
-    
+    itemprop = CharField(
+        max_length=32,
+        blank=True,
+        help_text="For schema.org microdata")
+
     class Meta:
         ordering = ["-weight"]
-    
+
     def __unicode__(self):
         return " - ".join((self.type, self.value))
-    
+
 
 #Resume > Section > Subsection > Detail
 
 class Resume(Model):
     #resume name, unique. Used for ID (HTML)
-    name = SlugField(max_length=32, unique=True, help_text="Unique name of resume")
-    
+    name = SlugField(
+        max_length=32,
+        unique=True,
+        help_text="Unique name of resume")
+
     #What it's called
-    title = CharField(max_length=64, help_text="Resume Title")
-    
+    title = CharField(
+        max_length=64,
+        help_text="Resume Title")
+
     #Which order the resume is displayed.
     #Resume with highest weight is shown on /resume page
-    weight = IntegerField(default=0, choices=NUM_CHOICES, help_text="Resume with highest weight is displayed on /resume")
-    
+    weight = IntegerField(
+        default=0,
+        choices=NUM_CHOICES,
+        help_text="Resume with highest weight is displayed on /resume")
+
     #HTML for resume's contact info section
-    contactInfo = TextField(blank=True, help_text="Contact information (displayed in header, HTML allowed)")
-    
+    contactInfo = TextField(
+        blank=True,
+        help_text="Contact information (displayed in header, HTML allowed)")
+
     #HTML for resume's footer section
-    footer = TextField(blank=True, help_text="Resume footer (HTML allowed)")
-    
+    footer = TextField(
+        blank=True,
+        help_text="Resume footer (HTML allowed)")
+
     #PDF file for resume
-    pdf = FileField(upload_to=get_pdf_path, blank=True, help_text="PDF version")
-    
+    pdf = FileField(
+        upload_to=get_pdf_path,
+        blank=True,
+        help_text="PDF version")
+
     #sections = ManyToManyField - ResumeSection
-    
+
     class Meta:
         ordering = ['-weight']
-    
+
     def __unicode__(self):
         return self.title
-    
+
     @models.permalink
     def get_absolute_url(self):
         return ('jsutlovic-resume-name', [str(self.name)])
-    
+
+
 class ResumeSection(Model):
     #Unique name of the section (used for HTML)
-    name = SlugField(max_length=64, unique=True, help_text="Unique section name")
-    
+    name = SlugField(
+        max_length=64,
+        unique=True,
+        help_text="Unique section name")
+
     #Title of the section
-    title = CharField(max_length=64, help_text="Section title (text displayed)")
-    
+    title = CharField(
+        max_length=64,
+        help_text="Section title (text displayed)")
+
     #Weight of the section (where it's displayed)
-    weight = IntegerField(default=0, choices=NUM_CHOICES, help_text="For ordering")
-    
+    weight = IntegerField(
+        default=0,
+        choices=NUM_CHOICES,
+        help_text="For ordering")
+
     #The resume the section is associated with
-    resume = ManyToManyField('Resume', related_name='sections', help_text="Related resume")
-    
+    resume = ManyToManyField(
+        'Resume',
+        related_name='sections',
+        help_text="Related resume")
+
     #subsections = ForeignKey - ResumeSubSection
-    
+
     def __unicode__(self):
         return self.title
-    
+
     class Meta:
         ordering = ['-weight']
-        
+
 
 class ResumeSubSection(Model):
     #Subsection name (used for HTML)
     name = CharField(max_length=64, help_text="Unique name")
-    
+
     #Title of the subsection
-    title = CharField(max_length=128, help_text="Subsection title (text displayed)")
-    
+    title = CharField(
+        max_length=128,
+        help_text="Subsection title (text displayed)")
+
     #Weight of the section (where it's displayed)
     weight = IntegerField(default=0, choices=NUM_CHOICES, help_text="Ordering")
-    
-    #How do we display dates? (Options to pass to Django's built in date filter)
-    dateDisplay = CharField(max_length=16, blank=True, choices=DATE_CHOICES, help_text="Date display formatting (Used with Django date filter)")
-    
+
+    #How we display dates (Options to pass to Django's built in date filter)
+    dateDisplay = CharField(
+        max_length=16,
+        blank=True,
+        choices=DATE_CHOICES,
+        help_text="Date display formatting (Used with Django date filter)")
+
     #Date of start (if any)
     dateFrom = DateField(null=True, blank=True, help_text="Start date")
-    
+
     #Date of end (if any)
     dateTo = DateField(null=True, blank=True, help_text="End date")
-    
+
     #The section it's related to
-    section = ForeignKey('ResumeSection', related_name='subsections', help_text="Related section")
-    
+    section = ForeignKey(
+        'ResumeSection',
+        related_name='subsections',
+        help_text="Related section")
+
     #details = ForeignKey - ResumeDetail
-    
+
     def __unicode__(self):
         return self.title
-    
+
     #Default order is by descending weight
     class Meta:
         ordering = ['-weight']
-    
-    
+
+
 class ResumeDetail(Model):
     #Weight of the detail
     weight = IntegerField(default=0, choices=NUM_CHOICES, help_text="Ordering")
-    
+
     #Detail contents
     contents = CharField(max_length=256, help_text="Contents (text displayed)")
-    
+
     #Related subsection
-    subsection = ForeignKey('ResumeSubSection', related_name='details', help_text="Related subsection")
-    
+    subsection = ForeignKey(
+        'ResumeSubSection',
+        related_name='details',
+        help_text="Related subsection")
+
     #Show as subsection - contents
     def __unicode__(self):
         return " - ".join([self.subsection.name, self.contents])
-    
+
     #Order by descending weight
     class Meta:
         ordering = ['-weight']
@@ -221,39 +289,54 @@ class ResumeDetail(Model):
 
 class Project(Model):
     #Project shortname
-    name = SlugField(max_length=32, unique=True, help_text="Shortname for the project")
-    
+    name = SlugField(
+        max_length=32,
+        unique=True,
+        help_text="Shortname for the project")
+
     #Title (displayed name) of project
-    title = CharField(max_length=64, help_text="Title (displayed name) of project")
-    
+    title = CharField(
+        max_length=64,
+        help_text="Title (displayed name) of project")
+
     #Description of the project
     description = TextField(help_text="Project description")
-    
+
     #URL of the project, if any
     url = URLField(blank=True, help_text="URL to project")
-    
+
     #How the dates should be displayed (if at all)
-    dateDisplay = CharField(max_length=16, blank=True, choices=DATE_CHOICES, help_text="Date formatting: Year/month and year/etc.")
-    
+    dateDisplay = CharField(
+        max_length=16,
+        blank=True,
+        choices=DATE_CHOICES,
+        help_text="Date formatting: Year/month and year/etc.")
+
     #Date of start (if any)
-    dateFrom = DateField(null=True, blank=True, help_text="Date project started")
-    
+    dateFrom = DateField(
+        null=True,
+        blank=True,
+        help_text="Date project started")
+
     #Date of end (else consider current)
-    dateTo = DateField(null=True, blank=True, help_text="Date project completed")
-    
+    dateTo = DateField(
+        null=True,
+        blank=True,
+        help_text="Date project completed")
+
     #The weight of the project
     weight = IntegerField(default=0, choices=NUM_CHOICES, help_text="Ordering")
-    
+
     #tags = ManyToMany - ProjectTechTag
     #images = ForeignKey - ProjectImage
     #icon = ProjectIcon
-    
+
     def __unicode__(self):
-        return self.title 
-    
+        return self.title
+
     def get_tags(self):
         return self.tags.all().order_by('name')
-    
+
     def get_images(self):
         return self.images.all().order_by('-weight')
 
@@ -261,10 +344,13 @@ class Project(Model):
 class ProjectTechTag(Model):
     #Name of the tag
     name = SlugField(max_length=32, unique=True, help_text="Tag name")
-    
+
     #Related project
-    projects = ManyToManyField('Project', related_name='tags', help_text="Related projects")
-    
+    projects = ManyToManyField(
+        'Project',
+        related_name='tags',
+        help_text="Related projects")
+
     def __unicode__(self):
         return self.name
 
@@ -272,39 +358,39 @@ class ProjectTechTag(Model):
     def get_absolute_url(self):
         return ('jsutlovic-projects-techtag', [str(self.name)])
 
-    
+
 class ProjectImage(Model):
     #A short image description
     title = CharField(max_length=128, help_text="Image description")
-    
+
     #Related project
-    project = ForeignKey('Project', related_name='images', help_text="Related project")
-    
+    project = ForeignKey(
+        'Project',
+        related_name='images',
+        help_text="Related project")
+
     #The image file
     image = ImageField(upload_to=get_image_path, help_text="Image file")
-    
+
     #For display ordering
     weight = IntegerField(default=0, choices=NUM_CHOICES, help_text="Ordering")
-    
+
     class Meta:
         ordering = ['-weight']
-        
+
     def __unicode__(self):
         return self.title
-    
-    
+
+
 class ProjectIcon(Model):
     #Related project this thumbnail is for
-    project = OneToOneField('Project', related_name='icon', help_text="Related project")
-    
+    project = OneToOneField(
+        'Project',
+        related_name='icon',
+        help_text="Related project")
+
     #The image file
     image = ImageField(upload_to=get_image_path, help_text="Image file")
-    
+
     def __unicode__(self):
         return "%s %s" % (self.project.name, "icon")
-
-
-
-
-
-
