e8cc5c8e00bf39e0672b65069dfae3c1e888c607
Jero Sutlovic <jsutlovic@gmail.com>
2013-01-13 02:45:31 -0500

    PEP8

--- main/sitemaps.py
+++ main/sitemaps.py
@@ -5,61 +5,66 @@ from main.models import Page, Resume, ProjectTechTag
 
 LASTMOD = date(2012, 6, 4)
 
+
 class StaticSitemap(Sitemap):
-    changefreq="monthly"
-    
+    changefreq = "monthly"
+
     def items(self):
         return settings.SITEMAP_STATICS
-    
+
     def lastmod(self, page):
         return LASTMOD
-    
+
     def location(self, page):
         return page[0]
-    
+
     def priority(self, page):
         return page[1]
 
+
 class PageSitemap(Sitemap):
-    changefreq="monthly"
-    priority=0.9
-    
+    changefreq = "monthly"
+    priority = 0.9
+
     def items(self):
         return Page.objects.all()
-    
+
     def lastmod(self, page):
         return LASTMOD
+
 
 class ResumeSitemap(Sitemap):
     changefreq = "monthly"
     priority = 0.6
-    
+
     def items(self):
         return Resume.objects.all()
-    
+
     def lastmod(self, resume):
         return LASTMOD
+
 
 class ResumePlainSitemap(Sitemap):
     changefreq = "monthly"
     priority = 0.5
-    
+
     def items(self):
         return Resume.objects.all()
-        
+
     def lastmod(self, resume):
         return LASTMOD
-    
+
     def location(self, resume):
-        return resume.get_absolute_url()+"/plain"
+        return resume.get_absolute_url() + "/plain"
+
 
 class ProjectTechtagSitemap(Sitemap):
     changefreq = "monthly"
     priority = 0.4
-    
+
     def items(self):
         return ProjectTechTag.objects.all()
-        
+
     def lastmod(self, tag):
         return LASTMOD
 
@@ -70,4 +75,3 @@ sitemaps = {
     'plainresumes': ResumePlainSitemap,
     'projecttags': ProjectTechtagSitemap,
 }
-    
