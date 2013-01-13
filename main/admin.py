ed8e9875f587bccf2b18c59525749d9f539f2000
Jero Sutlovic <jsutlovic@gmail.com>
2013-01-13 02:47:03 -0500

    PEP8

--- main/admin.py
+++ main/admin.py
@@ -1,12 +1,16 @@
-from main.models import *
+from main.models import Page, PageLink, SiteLink, ContactDetail, Resume, \
+    ResumeSection, ResumeSubSection, ResumeDetail, Project, ProjectTechTag, \
+    ProjectIcon, ProjectImage
 from django.contrib import admin
 from django.contrib.admin import ModelAdmin, StackedInline, TabularInline
+
 
 class PageAdmin(ModelAdmin):
     list_display = ('name', 'title')
     ordering = ('name',)
 
 admin.site.register(Page, PageAdmin)
+
 
 class PageLinkAdmin(ModelAdmin):
     list_display = ('name', 'title', 'url', 'weight', 'disabled')
@@ -15,12 +19,15 @@ class PageLinkAdmin(ModelAdmin):
 
 admin.site.register(PageLink, PageLinkAdmin)
 
+
 class SiteLinkAdmin(ModelAdmin):
-    list_display = ('name', 'title', 'url', 'weight', 'primaryLinks', 'secondaryLinks', 'disabled')
+    list_display = ('name', 'title', 'url', 'weight',
+                    'primaryLinks', 'secondaryLinks', 'disabled')
     list_display_links = ('name', 'title', 'url', 'weight')
-    ordering= ('-primaryLinks', '-weight', 'name')
+    ordering = ('-primaryLinks', '-weight', 'name')
 
 admin.site.register(SiteLink, SiteLinkAdmin)
+
 
 class ContactDetailAdmin(ModelAdmin):
     list_display = ('type', 'value', 'url', 'weight')
@@ -34,14 +41,16 @@ admin.site.register(ContactDetail, ContactDetailAdmin)
 class ResumeAdmin(ModelAdmin):
     list_display = ('name', 'title', 'weight', 'pdf')
     list_display_links = ('name', 'title', 'weight')
-    
+
 admin.site.register(Resume, ResumeAdmin)
+
 
 class ResumeSectionAdmin(ModelAdmin):
     list_display = ('name', 'title', 'weight')
     list_display_links = ('name', 'title', 'weight')
-    
+
 admin.site.register(ResumeSection, ResumeSectionAdmin)
+
 
 class ResumeSubSectionAdmin(ModelAdmin):
     list_display = ('name', 'title', 'weight', 'section')
@@ -49,6 +58,7 @@ class ResumeSubSectionAdmin(ModelAdmin):
     ordering = ('section', '-weight', )
 
 admin.site.register(ResumeSubSection, ResumeSubSectionAdmin)
+
 
 class ResumeDetailAdmin(ModelAdmin):
     list_display = ('contents', 'weight', 'subsection')
@@ -63,25 +73,29 @@ admin.site.register(ResumeDetail, ResumeDetailAdmin)
 class ProjectIconInline(StackedInline):
     model = ProjectIcon
     extra = 1
-    
+
     #Don't allow deleting icons
     def has_delete_permission(self, request, obj=None):
         return False
 
+
 class ProjectTechTagInline(TabularInline):
     model = ProjectTechTag.projects.through
     extra = 1
+
     def has_add_permission(self, request):
         return True
+
 
 class ProjectImageInline(TabularInline):
     model = ProjectImage
     extra = 1
 
+
 class ProjectAdmin(ModelAdmin):
     list_display = ('name', 'title', 'weight')
     list_display_links = ('name', 'title', 'weight')
-    ordering = ('-weight','-dateTo')
+    ordering = ('-weight', '-dateTo')
     inlines = (ProjectIconInline, ProjectTechTagInline, ProjectImageInline)
 
 admin.site.register(Project, ProjectAdmin)
@@ -89,6 +103,5 @@ admin.site.register(Project, ProjectAdmin)
 
 class ProjectTechTagAdmin(ModelAdmin):
     list_display = ('name',)
-    
-admin.site.register(ProjectTechTag, ProjectTechTagAdmin)
 
+admin.site.register(ProjectTechTag, ProjectTechTagAdmin)
